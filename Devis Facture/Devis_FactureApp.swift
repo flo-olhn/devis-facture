f96b6a3ac9f2f15b847d65f10e0fce22a7ab0616
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-10-24 13:55:16 +0200

    Initial Commit

---
+++ Devis Facture/Devis_FactureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Devis_FactureApp.swift
+//  Devis Facture
+//
+//  Created by Florian Ouilhon on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Devis_FactureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Devis FactureTests/Devis_FactureTests.swift
@@ -0,0 +1,17 @@
+//
+//  Devis_FactureTests.swift
+//  Devis FactureTests
+//
+//  Created by Florian Ouilhon on 24/10/2024.
+//
+
+import Testing
+@testable import Devis_Facture
+
+struct Devis_FactureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
